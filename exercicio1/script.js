a46a75dd0022b10d800f2eb114dbd6965920453a
EvertonSouzaFerreira <everton.souzaferreira@outlook.com>
2022-08-29 20:58:29 +0200

    exercicioJs01

---
+++ exercicio1/script.js
@@ -0,0 +1,13 @@
+let nome;
+let idade;
+
+nome = prompt('Qual seu nome?')
+idade = prompt('Qual sua idade?')
+
+
+
+console.log(typeof(nome))
+console.log(typeof(idade))
+
+
+console.log('Ola', nome, 'voce tem', idade, 'anos' )
